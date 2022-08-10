ef342d69cee7a01082e74bc08ebb15f20b16ef62
LIU Zhao <liuzhaomax@163.com>
2022-08-11 03:47:44 +1000

    add lang tag

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -61,7 +61,11 @@ function HomeLayout() {
 				<img id="sky" src={bgSky} alt="sky img" data-depth="0.03" />
 				<img id="back-rock" src={bgBackRock} alt="back rock img" data-depth=".05" />
 				<h4 id="slogan1" data-depth="0">配置CI/CD DevOps流程的微服务架构Web应用</h4>
-				<h4 id="slogan2" data-depth="0">Golang + React</h4>
+				<div id="slogan2" data-depth="0">
+					<img src="https://img.shields.io/badge/Golang-00a7d0?style=flat-square&logo=go&logoColor=white" alt="go"/>
+					<img src="https://img.shields.io/badge/-JavaScript-red?style=flat-square&logo=javascript&logoColor=white" alt="js"/>
+					{/*<img src="https://img.shields.io/github/release/liuzhaomax/maxblog-fe-main.svg?style=flat-square" alt="release"/>*/}
+				</div>
 				<h6 id="instruction" data-depth="0">GitHub开源项目</h6>
 				{/*<Button id="explore" shape="round" onClick={explore} data-depth="0">浏览</Button>*/}
 				<GithubOutlined id="explore" shape="round" onClick={explore} data-depth="0" />
