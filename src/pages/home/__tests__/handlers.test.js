64523211a7a10df81c6f5b22d028d15f1751729b
LIU Zhao <liuzhaomax@163.com>
2022-08-29 22:40:37 +1000

    sonar coverage bug

---
+++ src/pages/home/__tests__/handlers.test.js
@@ -0,0 +1,15 @@
+import {getContributors} from "../handlers"
+import axios from "axios"
+
+jest.mock("axios")
+
+test("test get contributor function", done => {
+    const response = new Promise((resolve, reject) => {
+        resolve(1)
+        reject(0)
+    })
+    axios.get.mockResolvedValue(response)
+    const result = getContributors()
+    expect(result).resolves.toBe(1)
+    done()
+})
